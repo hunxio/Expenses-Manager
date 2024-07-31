eef1f4941e07015da66564a95434db656c6f66c9
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-31 16:45:54 +0200

    formatting and added typehints

--- file_manager.py
+++ file_manager.py
@@ -1,20 +1,26 @@
+# Root files
 import Constants
+
+# Modules
 import os
 import pandas as pd
 
+
+# Checks if the file exists in the path or not
 def path_existance() -> bool:
     path = Constants.CSV_FILE_PATH
     isFile = os.path.isfile(path)
     return isFile
 
 
+# Initializes and/or appends data to a .csv file depending on path_existances call
 def csv_file(data: dict) -> None:
     df = pd.DataFrame(data)
-    csv_file = 'data.csv'
+    csv_file = "data.csv"
     if not path_existance():
-        df.to_csv(csv_file, mode='a', index=False, header=True)
+        df.to_csv(csv_file, mode="a", index=False, header=True)
     else:
-        df.to_csv(csv_file, mode='a', index=False, header=False)
+        df.to_csv(csv_file, mode="a", index=False, header=False)
 
 
 if __name__ == "__main__":
