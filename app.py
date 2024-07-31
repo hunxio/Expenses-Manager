9fc58647d0e41b82bda3b75836f72a03d5ea74bc
Huno <136697789+hunxio@users.noreply.github.com>
2024-07-31 15:42:29 +0200

    Init

---
+++ app.py
@@ -0,0 +1,9 @@
+from interface import gui
+
+
+def main():
+    gui()
+
+
+if __name__ == "__main__":
+    main()
---
+++ interface.py
@@ -0,0 +1,51 @@
+# Root files
+import Constants
+import utils
+# Modules
+import customtkinter
+import os
+from PIL import Image
+
+
+def gui():
+    app = customtkinter.CTk()
+    customtkinter.set_appearance_mode("system")
+    
+    # Icon Image
+    app.iconbitmap("media/icon.ico")
+
+    # Logo Image on side bar
+    logo = customtkinter.CTkImage(light_image=Image.open(Constants.LOGO_IMAGE_PATH),
+                                  dark_image=Image.open(Constants.LOGO_IMAGE_PATH),
+                                  size=(120, 120))
+    logo_label = customtkinter.CTkLabel(app, image=logo, text="")
+    logo_label.grid(row=0, column=0, padx=0, pady=20)
+
+
+    # Window Settings
+    app.title("Gestore di spese")
+    app.geometry("650x550")
+
+
+    # Text Label side bar
+    side_title_label = customtkinter.CTkLabel(app, text="Manager Spese", fg_color="transparent", font=("Montserrat", 18))
+    side_title_label.grid(row=1, column=0, padx=10, pady=0)
+
+
+    # Button Widgets
+    button_add_expense = customtkinter.CTkButton(app, text="Aggiungi spesa", command=lambda: utils.add_expense(app))
+    button_add_expense.grid(row=2, column=0, padx=10, pady=15)
+
+    button_check_expense = customtkinter.CTkButton(app, text="Controlla spese", command=None)
+    button_check_expense.grid(row=3, column=0, padx=10, pady=15)
+
+    button_check_expense = customtkinter.CTkButton(app, text="Spesa Mensile", command=None)
+    button_check_expense.grid(row=4, column=0, padx=10, pady=15)
+
+
+    app.mainloop()
+    
+
+
+if __name__ == "__main__":
+    gui()
