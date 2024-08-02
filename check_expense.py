bda417590306a1c8c76b4ac83a3127a0172efccf
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-02 16:21:14 +0200

    Error message to handle file missing error

--- check_expense.py
+++ check_expense.py
@@ -8,5 +8,10 @@ import pandas as pd
 def check_expense(application: object) -> None:
     # Delets columns content where column > 0
     clear_widgets(application)
-    headers, rows = open_csv_file("data.csv", "data.csv")
-    create_table(application, headers, rows)
+    try:
+        headers, rows = open_csv_file("data.csv", "data.csv")
+        create_table(application, headers, rows)
+    except (TypeError, FileNotFoundError):
+        application.columnconfigure(1, weight=1)
+        error_message_label = customtkinter.CTkLabel(application, text="Please add an expense to create the file.", fg_color="transparent", font=("Monsterrat", 18))
+        error_message_label.grid(row=0, column=1, padx=0, pady=0, sticky="WE")
