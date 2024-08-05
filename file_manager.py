e6edc3d85c55648a8df01f37de2c96e5b81f1e97
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 17:58:45 +0200

    added get_category_value, it needs to be fixed though, values not updating

--- file_manager.py
+++ file_manager.py
@@ -32,18 +32,23 @@ def get_data(file_path: str, file: str) -> list:
         return headers, rows
 
 
-def get_category_value(file_path: str, file: str) -> list:
+def get_category_value(file_path: str) -> list:
     with open(file_path, mode='r', newline='') as file:
         chart_data = []
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
         row = list(csv_reader)
-        for i in range(len(row)):
-            category, value = row[i][1], row[i][2]
-            chart_data.append([category, value])
+        for expense in row:
+            category, value = expense[1], float(expense[2])
+            if chart_data == []:
+                chart_data.append([category, value])
+            elif chart_data != []:
+                for _ in range(len(chart_data)):
+                    items_category, items_value = chart_data[_][0], chart_data[_][1]
+                    #TODO: FIX ITEMS_VALUE NOT UPDATING AFTER CALCULATING HOW MUCH IT IS
+                    if category == items_category:
+                        items_value = float(items_value) + float(value)
         return chart_data
 
 
-
-if __name__ == "__main__":
-    main()
+get_category_value("data.csv")
