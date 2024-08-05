be3ee344837f92022579406312e262c8b0ceebe5
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 18:53:37 +0200

    It now creates the graphic image, but not in the application frame

--- graphic_chart.py
+++ graphic_chart.py
@@ -6,8 +6,16 @@ from file_manager import get_category_value
 
 def make_graph(application: object) -> None:
     try:
-        print(get_category_value("data.csv"))
+        data_chart = get_category_value("data.csv")
     except FileNotFoundError:
         application.columnconfigure(1, weight=1)
         error_message_label = customtkinter.CTkLabel(application, text="Please add an expense to create a graph.", fg_color="transparent", font=("Monsterrat", 18))
         error_message_label.grid(row=0, column=1, padx=0, pady=0, sticky="WE")
+
+    category, value = [], []
+    for items in data_chart:
+        category.append(items[0])
+        value.append(items[1])
+
+    figure = pyplot.figure(figsize=(10, 7))
+    pyplot.pie(value, labels=category)
