d9b460790f5b05ba08cee4adff1eb342c3155ec6
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 18:42:51 +0200

    updated function, now it adds/update values properly to the list

--- file_manager.py
+++ file_manager.py
@@ -33,21 +33,23 @@ def get_data(file_path: str, file: str) -> list:
 
 
 def get_category_value(file_path: str) -> list:
+
     with open(file_path, mode='r', newline='') as file:
-        chart_data = []
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
         row = list(csv_reader)
+
+        chart_data = []
         for expense in row:
             category, value = expense[1], float(expense[2])
-            if chart_data == []:
+            found = False
+            for item in chart_data:
+                if item[0] == category:
+                    item[1] += value
+                    found = True
+                    break
+            if not found:
                 chart_data.append([category, value])
-            elif chart_data != []:
-                for _ in range(len(chart_data)):
-                    items_category, items_value = chart_data[_][0], chart_data[_][1]
-                    #TODO: FIX ITEMS_VALUE NOT UPDATING AFTER CALCULATING HOW MUCH IT IS
-                    if category == items_category:
-                        items_value = float(items_value) + float(value)
         return chart_data
 
 
