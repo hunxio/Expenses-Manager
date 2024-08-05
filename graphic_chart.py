750aa2e5606677c8e3b617a69d1c967c5c1a1a15
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 17:59:20 +0200

    function now correctly asks for only one parameter which is the relative path for the file

--- graphic_chart.py
+++ graphic_chart.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot
 import numpy
 
+from file_manager import get_category_value
+
 def make_graph(application: object) -> None:
-    ...
+    print(get_category_value("data.csv"))
