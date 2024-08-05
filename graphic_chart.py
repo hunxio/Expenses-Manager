b1cb2bad9c22426fd3e0fdea427d699a7fbad72e
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 18:43:05 +0200

    added error handling if file does not exist

--- graphic_chart.py
+++ graphic_chart.py
@@ -1,7 +1,13 @@
+import customtkinter
 from matplotlib import pyplot
 import numpy
 
 from file_manager import get_category_value
 
 def make_graph(application: object) -> None:
-    print(get_category_value("data.csv"))
+    try:
+        print(get_category_value("data.csv"))
+    except FileNotFoundError:
+        application.columnconfigure(1, weight=1)
+        error_message_label = customtkinter.CTkLabel(application, text="Please add an expense to create a graph.", fg_color="transparent", font=("Monsterrat", 18))
+        error_message_label.grid(row=0, column=1, padx=0, pady=0, sticky="WE")
