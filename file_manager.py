be4a8e95580d6a4f014535ae4671e09ed4fa6c83
Huno <136697789+hunxio@users.noreply.github.com>
2024-08-05 16:54:00 +0200

    created loop to go through rows appending items to list

--- file_manager.py
+++ file_manager.py
@@ -32,5 +32,18 @@ def get_data(file_path: str, file: str) -> list:
         return headers, rows
 
 
+def get_category_value(file_path: str, file: str) -> list:
+    with open(file_path, mode='r', newline='') as file:
+        chart_data = []
+        csv_reader = csv.reader(file)
+        headers = next(csv_reader)
+        row = list(csv_reader)
+        for i in range(len(row)):
+            category, value = row[i][1], row[i][2]
+            chart_data.append([category, value])
+        return chart_data
+
+
+
 if __name__ == "__main__":
     main()
